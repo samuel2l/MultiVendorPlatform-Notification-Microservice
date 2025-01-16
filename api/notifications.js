5395450e6a0d9f23fda8cd031a8d11cb8baf7d37
Samuel Adams <sama29571@gmail.com>
2025-01-16 23:20:57 +0000

    fix notification error

--- api/notifications.js
+++ api/notifications.js
@@ -8,6 +8,10 @@ notificationRoutes = (app, channel) => {
     const service = new NotificationService();
 
     SubscribeMessage(channel, service)
+
+    app.get("/",auth,(req,res)=>{
+        res.status(200).json({"msg":"yeah"})
+    })
  
 }
 module.exports=notificationRoutes
